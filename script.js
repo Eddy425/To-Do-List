7f427046edc3e0f3bfd1e3146ba10ce1d40a7347
Aditya Satyarthi <aditya.satyarthi06@gmail.com>
2026-06-16 14:22:17 +0530

    fixed minor bugs
    fixed a minor bug where the CSS of the task List would get strike throughed.

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ function displayTasks(){
     
     taskHTML += 
       `
-      <div>${name}</div>
-      <div>${date}</div>
+      <div class="taskString">${name}</div>
+      <div class="taskString">${date}</div>
       <button onclick="markAsDone(${i})" class="js-done-btn done-btn">Done</button>
       <button onclick="todoList.splice(${i},1);
       displayTasks();" class="js-delete-btn delete-btn">Delete</button>
@@ -42,11 +42,10 @@ function markAsDone(){
   if (doneBtn.innerHTML === 'Done') {
     doneBtn.innerHTML = 'Did it';
     document.querySelector('.js-done-btn').classList.add('did-it');
-    document.querySelector('.taskList').classList.add('strikeThrough');
+    document.querySelector('.taskString').classList.add('strikeThrough');
   } else if (doneBtn.innerHTML === 'Did it') {
     doneBtn.innerHTML = 'Done';
-    document,querySelector('.js-done-btn').classList.remove
-    ('did-it');
-    document.querySelector('.taskList').classList.remove('StrikeThrough');
+    document.querySelector('.js-done-btn').classList.remove('did-it');
+    document.querySelector('.taskString').classList.remove('strikeThrough');
   }
 }
